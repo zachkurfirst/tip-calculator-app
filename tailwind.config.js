3e38ee7e3cc710e33e9da18d16fe76b51c91a961
Zach Kurfirst <zachmk11@gmail.com>
2024-04-17 15:40:18 -0400

    add colors and font from style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+      },
+    },
+    fontFamily: {
+      spacemono: ["Space Mono", "monospace"],
+    },
   },
   plugins: [],
 };
