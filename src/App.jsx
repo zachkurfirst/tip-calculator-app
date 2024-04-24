a7fccd79bd652a22f99e4fe1fa520ffbd9bed03c
Zach Kurfirst <zachmk11@gmail.com>
2024-04-24 16:49:21 -0400

    continue design on Display component

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import logo from './assets/logo.svg'
 const App = () => {
   return (
     <>
-      <main className="flex flex-col items-center gap-4 h-dvh border-2 border-black">
+      <main className="flex flex-col items-center gap-12 h-dvh">
         <img src={logo} alt="Splitter logo" className="mt-12" />
-        <div className="flex gap-8 max-w-screen-md bg-white p-6 rounded-3xl shadow-md">
+        <div className="flex gap-8 justify-center max-w-full sm:max-w-screen-md bg-white p-8 rounded-3xl shadow-md mx-8">
           <BillForm />
           <Display />
         </div>
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,23 +1,23 @@
 const Display = () => {
   return (
-    <div id="display" className="flex flex-col justify-between rounded-xl bg-very-dark-cyan w-1/2 p-6">
-      <div id="display-results">
-        <div id="display-row" className="flex justify-between items-center mb-8">
-          <div id="display-labels" className="text-lg">
-            <p className="text-white">Tip Amount</p>
-            <p className="text-light-grayish-cyan">/ person</p>
+    <div id="display" className="flex flex-col justify-between rounded-xl bg-very-dark-cyan p-8 w-1/2">
+      <div id="display-results" className="flex flex-col gap-8 mt-4">
+        <div id="display-row" className="flex justify-between items-center">
+          <div id="display-labels" className="">
+            <p className="text-white text-sm">Tip Amount</p>
+            <p className="text-grayish-cyan text-xs">/ person</p>
           </div>
           <p className="text-strong-cyan text-4xl">$0.00</p>
         </div>
         <div id="display-row" className="flex justify-between items-center">
-          <div id="display-labels" className="text-lg">
-            <p className="text-white">Total</p>
-            <p className="text-light-grayish-cyan">/ person</p>
+          <div id="display-labels" className="">
+            <p className="text-white text-sm">Total</p>
+            <p className="text-grayish-cyan text-xs">/ person</p>
           </div>
           <p className="text-strong-cyan text-4xl">$0.00</p>
         </div>
       </div>
-      <button className="bg-strong-cyan text-very-dark-cyan uppercase rounded-md w-full py-1 hover:bg-light-grayish-cyan text-lg">Reset</button>
+      <button className="bg-strong-cyan text-very-dark-cyan uppercase rounded-md py-2 enabled:hover:bg-light-grayish-cyan text-lg transition-all disabled:opacity-20">Reset</button>
     </div>
   );
 };
