26b974e8a4e5c625c5cd534f1cf170b0cda374fd
Zach Kurfirst <zachmk11@gmail.com>
2024-05-10 13:29:55 -0400

    ternary on bill and people inputs display red error ring only on error

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,6 +6,8 @@ const Display = ({ tipPP, totalPP, handleResetBtn }) => {
       currency: "USD",
     }).format(amt);
 
+  // TODO: round display amounts to 2 decimal places
+
   return (
     <div
       id="display"
